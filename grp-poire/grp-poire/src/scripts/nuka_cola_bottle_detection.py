e601a17639cd8bb5bc6a000f1ef1332d1c26e7ca
Marianne <mariannedepoorter@hotmail.fr>
2022-01-14 11:26:11 +0100

    published some updates on marker publising on rviz

--- grp-poire/grp-poire/src/scripts/nuka_cola_bottle_detection.py
+++ grp-poire/grp-poire/src/scripts/nuka_cola_bottle_detection.py
@@ -40,11 +40,15 @@ class BottleDetection():
             '/bottle',
             Marker, queue_size=10
         )
+        self.markers_list = list()
 
     def detectAndDisplay(self, frame):
         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         frame_gray = cv.equalizeHist(frame_gray)
         color_info = (255, 255, 255)
+        camera_width = 1920.0
+        camera_height = 1080.0
+        hfov = 64
 
         # -- Detect bottles
         bottles = self.nuka_cascade.detectMultiScale(
@@ -63,19 +67,20 @@ class BottleDetection():
                     for pixel in row:
                         if pixel < distance and pixel != 0:
                             distance = pixel  # ros distance with realsense camera
-                # cv.waitKey(800)
-                # cv.destroyAllWindows()
                 # D = H / sin(a) -> sin(a) = H / D -> a = arcsin(H/D)
-                angle = math.asin(w/distance)
+                #angle = math.asin(w/distance)
+                horizontal_angle_from_middle = ((x+w - camera_width/2)/(camera_width/2))*(hfov/2) * math.pi / 180
+                print("!!! ANGLE : " + str(horizontal_angle_from_middle))
                 cv.putText(frame, "{:f}".format(
                     distance), (x, y), cv.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv.LINE_AA)
                 cv.putText(frame, "{:f}".format(
-                    angle), (x+w, y+h), cv.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv.LINE_AA)
-                #cv.imshow('Capture - Cola detection', frame)
-                # cv.waitKey(800)
+                    horizontal_angle_from_middle ), (x+w, y+h), cv.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv.LINE_AA)
+                cv.imshow('Capture - Cola detection', frame)
+                #cv.waitKey(800)
+        
                 estimated_pose = Pose()
-                estimated_pose.position.x = distance / 1000
-                estimated_pose.position.y = 0
+                estimated_pose.position.x = distance / 1000 * math.cos(horizontal_angle_from_middle)
+                estimated_pose.position.y = distance / 1000 * math.sin(horizontal_angle_from_middle) # equals distance * sin(angle from middle of camera)
                 self.handle_markers(estimated_pose)
 
     def odomCoordinates(self, data: Odometry):
@@ -103,7 +108,7 @@ class BottleDetection():
         self.detectAndDisplay(cv_image)
 
     def handle_markers(self, pose: Pose):
-        self.markers_list = []
+        
         '''if marker is not in area of already existing marker
                 then publish marker
             else if position estimated has changed significantly
@@ -114,15 +119,17 @@ class BottleDetection():
             marker_to_publish = self.generateMarker(pose)
             self.markerPublisher.publish(marker_to_publish)
             self.markers_list.append(marker_to_publish)
+            #print(self.markers_list)
+            #print(len(self.markers_list))
             print("Marker Published because first one")
 
         for existing_marker in self.markers_list:
-            '''if not self.areInSameArea(existing_marker.pose, pose):
+            if not self.areInSameArea(existing_marker.pose, pose):
                 marker_to_publish = self.generateMarker(pose)
                 self.markerPublisher.publish(marker_to_publish)
                 self.markers_list.append(marker_to_publish)
-                print("Marker Published because not in area")'''
-            '''elif self.positionChangedSignificantly(existing_marker.pose, pose):
+                print("Marker Published because not in area")
+            elif self.positionChangedSignificantly(existing_marker.pose, pose):
                 existing_marker.action = Marker.DELETE
                 self.markerPublisher.publish(existing_marker)
                 self.markers_list.remove(existing_marker)
@@ -130,16 +137,22 @@ class BottleDetection():
                 newest_marker = self.generateMarker(pose)
                 self.markerPublisher.publish(newest_marker)
                 self.markers_list.append(newest_marker)
-                print("Marker Published because position changed")'''
+                print("Marker Published because position changed")
 
     def areInSameArea(self, existing_marker: Pose, new_marker: Pose):
-        a = numpy.array(existing_marker.position.x, existing_marker.position.y)
-        b = numpy.array(new_marker.position.x, new_marker.position.y)
+        a = numpy.array((existing_marker.position.x, existing_marker.position.y))
+        b = numpy.array((new_marker.position.x, new_marker.position.y))
         dist = numpy.linalg.norm(a-b)
-        return dist < 50
+        print("!!!! DISTANCE : " + str(dist) + " are in same area : " + str(dist<10))
+        return dist < 10
 
     def positionChangedSignificantly(self, existing_marker: Pose, new_marker: Pose):
-        return abs(existing_marker.position.x - new_marker.position.x) > 10 or abs(existing_marker.position.y - new_marker.position.y) > 10
+        diff_x = abs(existing_marker.position.x - new_marker.position.x)
+        diff_y = abs(existing_marker.position.y - new_marker.position.y)
+        print("delta x : " + str(diff_x) + "; delta y : " + str(diff_y))
+        bool_val = abs(existing_marker.position.x - new_marker.position.x) > 0.2 or abs(existing_marker.position.y - new_marker.position.y) > 0.4
+        #print("!!!! CHANGED : " + str(bool_val))
+        return bool_val
 
     def generateMarker(self, pose: Pose):
         pose_stamped = PoseStamped ()
@@ -153,7 +166,7 @@ class BottleDetection():
         marker.type = Marker.CUBE
         marker.action = Marker.ADD
         marker.pose = pose_stamped.pose
-        marker.scale.x = 1
+        marker.scale.x = 0.1
         marker.scale.y = 0.1
         marker.scale.z = 0.1
         marker.color.a = 1.0
