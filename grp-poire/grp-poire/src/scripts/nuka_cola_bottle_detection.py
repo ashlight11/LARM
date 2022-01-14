844125b8ff036deac61ac7510f9ffa23c305cbff
Marianne <mariannedepoorter@hotmail.fr>
2022-01-14 16:58:44 +0100

    end of challenge 2

--- grp-poire/grp-poire/src/scripts/nuka_cola_bottle_detection.py
+++ grp-poire/grp-poire/src/scripts/nuka_cola_bottle_detection.py
@@ -39,16 +39,13 @@ class BottleDetection():
         rospy.Subscriber("/odom", Odometry, self.odomCoordinates)
 
         # publisher for bottle markers
-        '''self.markerPublisher = rospy.Publisher(
-            '/bottle',
-            Marker, queue_size=10
-        )'''
         self.markerPublisher = rospy.Publisher(
             '/bottle',
-            MarkerArray, queue_size=10
+            Marker, queue_size=10
         )
+    
         self.markers_list = list()
-        self.marker_array_msg = MarkerArray()
+        
 
     def detectAndDisplay(self, frame):
         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -150,41 +147,37 @@ class BottleDetection():
         
         if len(self.markers_list) == 0:
             marker_to_publish = self.generateMarker(pose)
-            #self.markerPublisher.publish(marker_to_publish)
+            self.markerPublisher.publish(marker_to_publish)
             self.markers_list.append(marker_to_publish)
-            self.markerPublisher.publish(self.marker_array_msg)
             #print(self.markers_list)
             #print(len(self.markers_list))
             #print("Marker Published because first one")
 
         for existing_marker in self.markers_list:
-            if not self.areInSameArea(existing_marker.pose, pose):
+            if self.areNotInSameArea(existing_marker.pose, pose):
                 marker_to_publish = self.generateMarker(pose)
-                #self.markerPublisher.publish(marker_to_publish)
+                self.markerPublisher.publish(marker_to_publish)
                 self.markers_list.append(marker_to_publish)
-                self.markerPublisher.publish(self.marker_array_msg)
                 #print("Marker Published because not in area")
 
             elif self.positionChangedSignificantly(existing_marker.pose, pose):
                 # Delete current marker 
                 existing_marker.action = Marker.DELETE
-                #self.markerPublisher.publish(existing_marker)
+                self.markerPublisher.publish(existing_marker)
                 self.markers_list.remove(existing_marker)
-                self.marker_array_msg.markers.append(existing_marker)
-                self.markerPublisher.publish(self.marker_array_msg)
+                
                 #print("Marker Deleted because position changed")
                 newest_marker = self.generateMarker(pose)
-                #self.markerPublisher.publish(newest_marker)
+                self.markerPublisher.publish(newest_marker)
                 self.markers_list.append(newest_marker)
-                self.markerPublisher.publish(self.marker_array_msg)
                 #print("Marker Published because position changed")
 
-    def areInSameArea(self, existing_marker: Pose, new_marker: Pose):
+    def areNotInSameArea(self, existing_marker: Pose, new_marker: Pose):
         a = numpy.array((existing_marker.position.x, existing_marker.position.y))
         b = numpy.array((new_marker.position.x, new_marker.position.y))
         dist = numpy.linalg.norm(a-b)
-        print("!!!! DISTANCE : " + str(dist) + " are in same area : " + str(dist<10))
-        return dist < 5
+        #print("!!!! DISTANCE : " + str(dist) + " are in same area : " + str(dist<10))
+        return dist > 5
 
     def positionChangedSignificantly(self, existing_marker: Pose, new_marker: Pose):
         diff_x = abs(existing_marker.position.x - new_marker.position.x)
@@ -203,6 +196,7 @@ class BottleDetection():
         marker = Marker()
         marker.header.frame_id = "camera_link"
         marker.id = len(self.markers_list)
+        print("MARKER ID : " + str(marker.id))
         marker.type = Marker.CUBE
         marker.action = Marker.ADD
         marker.pose = pose_stamped.pose
@@ -213,7 +207,6 @@ class BottleDetection():
         marker.color.r = 0.0
         marker.color.g = 1.0
         marker.color.b = 0.0
-        self.marker_array_msg.markers.append(marker) 
         return marker
 
     '''def show_video(self):
