8082ee36eaf4df4077728341a9232cf565c4ca7c
Marianne De Poorter <mariannedepoorter@hotmail.fr>
2022-01-21 12:06:18 +0100

    code cleanup, refactor and documenting

--- grp-poire/grp-poire/scripts/marker_publisher.py
+++ grp-poire/grp-poire/scripts/marker_publisher.py
@@ -5,11 +5,13 @@ import tf
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Pose, PoseStamped
 
+
+# This class is dedicated to publishing green cube markers on rviz
 class MarkerPublisher():
     def __init__(self):
-        self.tfListener = tf.TransformListener()
+        self.tfListener = tf.TransformListener() 
         rospy.Subscriber("alert_bottle", Pose , self.handle_markers)
-        # publisher for bottle markers
+        # Publisher for bottle markers
         self.markerPublisher = rospy.Publisher(
             '/bottle',
             Marker, queue_size=10
@@ -19,7 +21,12 @@ class MarkerPublisher():
 
     def handle_markers(self, pose: Pose):
         
-        '''if marker is not in area of already existing marker
+        # Code logic for this function
+        '''
+        if list of markers is empty
+            then publish marker
+        for every marker in list of markers
+            if marker is not in area of already existing marker
                 then publish marker
             else if position estimated has changed significantly
                 then delete marker in area and publish new one
@@ -30,44 +37,38 @@ class MarkerPublisher():
             marker_to_publish = self.generateMarker(pose)
             self.markerPublisher.publish(marker_to_publish)
             self.markers_list.append(marker_to_publish)
-            #print(self.markers_list)
-            #print(len(self.markers_list))
-            #print("Marker Published because first one")
 
         for existing_marker in self.markers_list:
             if self.areNotInSameArea(existing_marker.pose, pose):
+                # Generate new marker
                 marker_to_publish = self.generateMarker(pose)
                 self.markerPublisher.publish(marker_to_publish)
                 self.markers_list.append(marker_to_publish)
-                #print("Marker Published because not in area")
 
             elif self.positionChangedSignificantly(existing_marker.pose, pose):
                 # Delete current marker 
                 existing_marker.action = Marker.DELETE
                 self.markerPublisher.publish(existing_marker)
                 self.markers_list.remove(existing_marker)
-                #print("Marker Deleted because position changed")
+                
+                # Generate new marker
                 newest_marker = self.generateMarker(pose)
                 self.markerPublisher.publish(newest_marker)
                 self.markers_list.append(newest_marker)
-                #print("Marker Published because position changed")
+                
 
+    # Compute distance between two positions to determine if object are close or not
     def areNotInSameArea(self, existing_marker: Pose, new_marker: Pose):
         a = numpy.array((existing_marker.position.x, existing_marker.position.y))
         b = numpy.array((new_marker.position.x, new_marker.position.y))
-        dist = numpy.linalg.norm(a-b)
-        #print("!!!! DISTANCE : " + str(dist) + " are in same area : " + str(dist<10))
-        return dist > 5
+        dist = numpy.linalg.norm(a-b) # euclidian distance
+        return dist > 3
 
+    # Compare positions to determine whether a change in position should be done or not
     def positionChangedSignificantly(self, existing_marker: Pose, new_marker: Pose):
-        diff_x = abs(existing_marker.position.x - new_marker.position.x)
-        diff_y = abs(existing_marker.position.y - new_marker.position.y)
-        #print("delta x : " + str(diff_x) + "; delta y : " + str(diff_y))
-        bool_val = abs(existing_marker.position.x - new_marker.position.x) > 0.2 or abs(existing_marker.position.y - new_marker.position.y) > 0.4
-        #print("!!!! CHANGED : " + str(bool_val))
-        return bool_val
+        return abs(existing_marker.position.x - new_marker.position.x) > 0.2 or abs(existing_marker.position.y - new_marker.position.y) > 0.4
 
-
+    # Generate a rviz Marker with desired properties
     def generateMarker(self, pose: Pose):
         pose_stamped = PoseStamped ()
         pose_stamped.pose = pose
@@ -92,7 +93,7 @@ class MarkerPublisher():
 
 if __name__ == '__main__':
     try:
-        # Initialize ROS::node
+        # Initialize ROS node
         rospy.init_node('Marker Publisher', anonymous=True)
         node = MarkerPublisher()
         rospy.spin()
--- grp-poire/grp-poire/scripts/mover_and_laser_v2.py
+++ grp-poire/grp-poire/scripts/mover_and_laser_v2.py
@@ -1,75 +1,80 @@
-from os import name
 import rospy
-from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist, Point32
 from sensor_msgs.msg import LaserScan, PointCloud
 from tf.transformations import euler_from_quaternion
-from nav_msgs.msg import Odometry
-from std_msgs.msg import Header
 import tf
 import math
-import numpy
 
 
-PUBLISHING_RATE = 0.1
-MODE_PARAMETER = "/simulation"
-goal_topic = '/move_base_simple/goal'
-cmd_topic = '/cmd_vel_mux/input/navi'
-rviz_topic = '/rviz_point_cloud'
-laser_topic = '/scan'
+PUBLISHING_RATE = 0.1 # The rate at which we publish navigation commands
+MODE_PARAMETER = "/simulation" # The boolean parameter we get from the launch file
+cmd_topic = '/cmd_vel_mux/input/navi'# The topic on which we publish velocity commands
+rviz_topic = '/rviz_point_cloud' # The topic on which we publish the point cloud
+laser_topic = '/scan' # The topic on which we receive the laser data
 
 
+# This class is dedicated to autonomous navigation and point publishing on rviz
 class AutonomousNav():
 
     def __init__(self):
-        self.tfListener = tf.TransformListener()
-        self.commands = Twist()
-        self.point_2D = PointCloud()
+        
+        self.commands = Twist() # Navigation commands
 
-        self.isTurning = False
+        self.point_2D = PointCloud() # PointCloud for obstacles
 
-        self.mode = rospy.get_param(MODE_PARAMETER)
+        self.isTurning = False # Flag to detect that a movement is being made
 
-        # publisher for nav commands
+        # Get the launch file parameter that specifies if we are in simulation
+        self.mode = rospy.get_param(MODE_PARAMETER) 
+
+        # Publisher for nav commands
         self.commandPublisher = rospy.Publisher(
             cmd_topic,
             Twist, queue_size=10)
 
-        if(self.mode):
-            #print("In simulation mode")
+        if(self.mode): # If in simulation
             self.FORWARD_SPEED_MPS = 2
-        else:
+        else: # Else in real life
             self.FORWARD_SPEED_MPS = 0.2
 
+        # Assign the first velocity commands
         self.commands.linear.x = self.FORWARD_SPEED_MPS
         self.commands.angular.z = 0.0
 
-        # publisher for 2D Points
+        # Publisher for 2D Points
         self.rvizPublisher = rospy.Publisher(
             rviz_topic,
             PointCloud, queue_size=10
         )
+        # Publish the point at rate PUBLISHING_RATE
+        rospy.Timer(rospy.Duration(PUBLISHING_RATE), self.display_points, oneshot=False)
 
         if(self.mode):
+            # In simulation, "laser_callback_sim" is called
             rospy.Subscriber(laser_topic, LaserScan, self.laser_callback_sim)
             rospy.Timer(rospy.Duration(PUBLISHING_RATE),
                         self.move_command, oneshot=False)
         else:
+            # IRL, "laser_callback" is called
             rospy.Subscriber(laser_topic, LaserScan, self.laser_callback)
 
+    # Publish commands at rate PUBLISHING_RATE while in simulation
     def move_command(self, data):
         if(not self.isTurning):
             self.commandPublisher.publish(self.commands)
 
+    # Publish commands at rate PUBLISHING_RATE while in simulation
     def display_points(self, data):
         self.point_2D.header.frame_id = 'base_link'
         self.rvizPublisher.publish(self.point_2D)
 
+    # Callback for laser data while in simulation
     def laser_callback_sim(self, data: LaserScan):
         isTurning = False
         self.point_2D.points.clear()
         obstacles = []
         angle = data.angle_min
+        # Compute the laser data to get coordinates relative to the robot
         for aDistance in data.ranges:
             if 0.1 < aDistance and aDistance < 2.0:
                 aPoint = Point32()
@@ -78,65 +83,62 @@ class AutonomousNav():
                 aPoint.z = 0.0
                 obstacles.append(aPoint)
             angle += data.angle_increment
+        # Assign each obstacle found to a Point Cloud
         for point in obstacles:
             self.point_2D.points.append(point)
+
+        # Front value in all the laser rays
         front = data.ranges[0]
 
+        # Left value in all the laser rays
         left = data.ranges[15]
 
+        # Right value in all the laser rays
         right = data.ranges[345]
-
-        '''print('-------------------------------------------')
-        print('Range data at 0 deg:   {}'.format(front))
-        print('Range data at 15 deg:  {}'.format(left))
-        print('Range data at 345 deg: {}'.format(right))
-        print('-------------------------------------------')'''
 
         thr1 = 0.6  # Laser scan range threshold
         thr2 = 1  # Turning threshold
 
-        # Checks if there are obstacles in front and
+        # Checks if there are obstacles in front and 15 degrees left and right
         if front > thr1 and left > thr2 and right > thr2 and not isTurning:
-            # 15 degrees left and right 
-            # go forward (linear velocity)
+            # If not, go forward (linear velocity)
             self.commands.linear.x = self.FORWARD_SPEED_MPS
             self.commands.angular.z = 0.0  # do not rotate (angular velocity)
         else:
-            if(front < thr1):
+            if(front < thr1): # obstacle in front
                 self.commands.linear.x = 0.0
-                if(left < right):
-                    self.commands.angular.z = data.angle_max
+                if(left < right): # more room at right
+                    self.commands.angular.z = data.angle_max # turn right
                     isTurning = True
-                else:
-                    self.commands.angular.z = - data.angle_max
-            if (left < thr2):
-
+                else: # more room at left
+                    self.commands.angular.z = - data.angle_max # turn left
+            if (left < thr2): # obstacle at left
                 self.commands.linear.x = 0.0  # stop
                 self.commands.angular.z = data.angle_max  # rotate counter-clockwise
                 isTurning = True
-            elif (right < thr2):
-
+            elif (right < thr2): # obstacle at right
                 self.commands.linear.x = 0.0  # stop
                 self.commands.angular.z = - data.angle_max  # rotate clockwise
                 isTurning = True
-
-            if front > thr1 and (left > thr2) and (right > thr2):
+            if front > thr1 and (left > thr2) and (right > thr2): # back to no danger
                 isTurning = False
                 self.commands.linear.x = self.FORWARD_SPEED_MPS
                 self.commands.angular.z = 0.0
 
+    # Callback function for IRL laser data
     def laser_callback(self, data: LaserScan):
         self.point_2D.points.clear()
         obstacles = []
         angle = data.angle_min
-        angle_left = 0
+        # Initialize angles of the object detected on both sides
+        # The aim is to detect the maximum turn to perform in order to 
+        # get the detected obstacle out of sight 
+        angle_left = 0 
         angle_right = 0
-        if(self.mode):  # if in simulation mode
-            thr1 = 1  # Laser scan range threshold
-
-        else:  # eventually adapt these parameters IRL
-            thr1 = 0.4  # Laser scan range threshold
         
+        thr = 0.3  # Laser scan range threshold to consider an object an obstacle
+        
+        # Go over all the laser rays
         for index, aDistance in enumerate(data.ranges):
             if 0.1 < aDistance and aDistance < 2.0:
                 aPoint = Point32()
@@ -146,53 +148,60 @@ class AutonomousNav():
                 obstacles.append(aPoint)
             angle += data.angle_increment
 
-            if(aDistance < thr1 and aDistance >= data.range_min):
+            # If object is a close obstacle but is not a wrong value
+            if(aDistance < thr and aDistance >= data.range_min):
+                # For values at right
                 if index in range(200, math.floor(len(data.ranges)/2)):
                     if ((index * 0.36 - 200 * 0.36) * math.pi / 180) > angle_right:
+                        self.isTurning = True
                         angle_right = (index * 0.36 - 200 *
                                        0.36) * math.pi / 180
-                    self.isTurning = True
+                # For values at left
                 elif index in range(math.floor(len(data.ranges)/2), 580):
-                    self.isTurning = True
                     if (abs((index * 0.36 - 580 * 0.36) * math.pi / 180)) > abs(angle_left):
+                        self.isTurning = True
                         angle_left = (index * 0.36 - 580 *
                                       0.36) * math.pi / 180
-                else:
-                    #self.isTurning = False
+                else: # No danger, increment slowly the speed up to max speed
                     if(self.commands.linear.x < self.FORWARD_SPEED_MPS) :
                         self.commands.linear.x += self.FORWARD_SPEED_MPS / 4
-                    else :
+                    else : # use max speed
                         self.commands.linear.x = self.FORWARD_SPEED_MPS
-                    self.commands.angular.z = 0.0
-            else:
-                #print("GO FORWARD")
+                    self.commands.angular.z = 0.0 # stop turning
+                    self.isTurning = False
+            else: # no need to turn
                 self.isTurning = False
+                # Increment up to max speed
                 if(self.commands.linear.x < self.FORWARD_SPEED_MPS) :
                     self.commands.linear.x += self.FORWARD_SPEED_MPS / 4
                 else :
                     self.commands.linear.x = self.FORWARD_SPEED_MPS
                 self.commands.angular.z = 0.0
-        #print("ANGLE LEFT " + str(angle_left) + " ANGLE RIGHT : " + str(angle_right))
+        # Handle on which side to actually turn
         if (angle_left != 0 and angle_right != 0):
+        # if we are in a corner (obstacles on both sides)
             self.isTurning = True
-            self.commands.angular.z = data.angle_max + 0.1
+            self.commands.angular.z = data.angle_max + 0.1 # turn right always
             self.commands.linear.x  = self.FORWARD_SPEED_MPS/4
         if abs(angle_left) > abs(angle_right):
+            # Bigger obstacle at left
             self.isTurning = True
-            self.commands.angular.z = angle_left + 0.1
+            self.commands.angular.z = angle_left + 0.1 # Turn left
             self.commands.linear.x = self.FORWARD_SPEED_MPS / 4
-        elif angle_right != 0.0:
+        elif angle_right != 0.0: # No obstacle left, so check if there is one on the right side
             self.isTurning = True
-            self.commands.angular.z = angle_right + 0.1
+            self.commands.angular.z = angle_right + 0.1 # Turn right
             self.commands.linear.x = self.FORWARD_SPEED_MPS / 4
         self.commandPublisher.publish(self.commands)
 
+        # Publish the Point Cloud to rviz
         for point in obstacles:
             self.point_2D.points.append(point)
 
 
 if __name__ == '__main__':
     rospy.init_node('Move and Avoid Obstacles', anonymous=True)
-    input("Press Enter to continue...")
+    # Wait for user key press, useful to setup screen recording before robot is moving
+    input("Press Enter to continue...") 
     node = AutonomousNav()
     rospy.spin()
